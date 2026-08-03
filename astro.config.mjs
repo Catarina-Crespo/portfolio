b261109d80a0bcecfcb44ba23a0873a017e09f26
Catarina Crespo <ci.crespo@campus.fct.unl.pt>
2026-08-03 02:19:24 +0100

    Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://catarina-crespo.github.io/portfolio/',
+  base: 'portfolio',
   integrations: [react()],
 
   i18n: {
